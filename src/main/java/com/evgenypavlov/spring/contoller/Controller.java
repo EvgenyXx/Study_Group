faad0da4c788cbdb1eb7f1f8f84ae19364d4340b
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-08-27 22:05:46 +0300

    first commit

--- src/main/java/com/evgenypavlov/spring/contoller/Controller.java
+++ src/main/java/com/evgenypavlov/spring/contoller/Controller.java
@@ -12,19 +12,19 @@ public class Controller {
     private final StreamService workerThreadService = new StreamService();
 
 
-    public void removeStudentByFIO (String firstName,String lastName,String middleName){
-        service.removeStudentByFIO(firstName,lastName,middleName);
+    public void removeStudentByFIO(String firstName, String lastName, String middleName) {
+        service.removeStudentByFIO(firstName, lastName, middleName);
     }
 
-    public List<Student> studentList(){
+    public List<Student> studentList() {
         return service.SortStudentById();
     }
 
-    public List<Student>sortStudentByFIO(){
-       return service.sortStudentByFIO();
+    public List<Student> sortStudentByFIO() {
+        return service.sortStudentByFIO();
     }
 
-    public List<StudyGroup> compareGroupsCount (){
-       return workerThreadService.compareGroupsCount();
+    public List<StudyGroup> compareGroupsCount() {
+        return workerThreadService.compareGroupsCount();
     }
 }
