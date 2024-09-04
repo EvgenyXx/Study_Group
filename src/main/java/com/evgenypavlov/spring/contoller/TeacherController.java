ff9283c3961c847563ae516d6d64114ff6fefbc0
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-09-04 15:09:07 +0300

    update

--- src/main/java/com/evgenypavlov/spring/contoller/TeacherController.java
+++ src/main/java/com/evgenypavlov/spring/contoller/TeacherController.java
@@ -2,26 +2,34 @@ package com.evgenypavlov.spring.contoller;
 
 import com.evgenypavlov.spring.model.data.teachers.Teachers;
 import com.evgenypavlov.spring.model.service.TeacherService;
+import com.evgenypavlov.spring.view.TeacherVIew;
 
 import java.util.List;
 
 public class TeacherController implements UserController<Teachers>{
-    private TeacherService teacherService;
+    private final TeacherService teacherService = new TeacherService();
+    private final TeacherVIew teacherVIew = new TeacherVIew();
+
+
     public void removeStudentByFIO (String firstName,String lastName,String middleName,String lesson){
         teacherService.removeStudentByFIO(firstName,lastName,middleName,lesson);
     }
 
     public List<Teachers> sortByTeacher(){
-       return teacherService.sortByTeacher();
+        List<Teachers>teachersList = teacherService.sortByTeacher();
+        teacherVIew.sendOnConsole(teachersList);
+       return teachersList;
     }
 
     public List<Teachers> sortTeacherByFIO(){
-        return teacherService.sortTeacherByFIO();
+        List<Teachers>teachers = teacherService.sortTeacherByFIO();
+        teacherVIew.sendOnConsole(teachers);
+        return teachers;
     }
 
-    public void crateStudent (String firstName,String lastName,String middleNam){
-        teacherService.crateTeacher(firstName,lastName,middleNam);
-    }
+//    public void crateStudent (String firstName,String lastName,String middleNam){
+//        teacherService.crateTeacher(firstName,lastName,middleNam);
+//    }
 
     @Override
     public void create(String firstName, String lastName, String middleName) {
---
+++ src/main/java/com/evgenypavlov/spring/view/TeacherVIew.java
@@ -0,0 +1,14 @@
+package com.evgenypavlov.spring.view;
+
+import com.evgenypavlov.spring.model.data.teachers.Teachers;
+
+import java.util.List;
+
+public class TeacherVIew implements UserView<Teachers> {
+    @Override
+    public void sendOnConsole(List<Teachers> studentList) {
+        for (Teachers teachers : studentList){
+            System.out.println(teachers.getLastName());
+        }
+    }
+}
